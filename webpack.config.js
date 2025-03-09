6592b68721b8d25d21b97f1038e8be44583d2b28
serioio321 <bykreev98@gmail.com>
2025-03-09 18:28:28 +0300

    mesto deploy

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
-    publicPath: '/mesto/',
+    publicPath: '',
   },
   mode: 'development',
   devtool: 'inline-source-map',
